55de5eac8cbe471f3699866568b8c4c50cc54fc4
Jinu0107 <wjdrms735@naver.com>
2021-03-11 20:52:24 +0900

    어드민 페이지 구현중

---
+++ pages/admin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- 
+        1. 알림
+        2. 회원가입 승인
+        3. 휴가 , 휴가취소 승인
+        4. 일정 등록 
+        5. 유저목록
+        6. 일정
+        -->
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
